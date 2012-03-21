d4d0b3652df7c97dadf11fb87480ea5fb281ed45
Mike Metral <mdmetra@sandia.gov>
2012-03-20 18:21:10 -0700

    Init commit of droidlandia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Droidlandia::Application.config.secret_token = '86c10bff6db08fa2d8cb9e5664252210917ab1c423858efce5cfad2897b8fa9dc408cbcc09bbc88fde0c9e722654813e9992854db492191ae08059da9b6da5ec'
