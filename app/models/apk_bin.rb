c892892fadb6bb8ce9c4c8fde8c2565b09ead6f8
Mike Metral <mdmetra@sandia.gov>
2012-03-20 20:56:05 -0700

    Added basic paperclip upload functionality

--- app/models/apk_bin.rb
+++ app/models/apk_bin.rb
@@ -1,7 +1,13 @@
+require 'paperclip'
+
 class ApkBin
   include MongoMapper::Document
+  include Paperclip::Glue
 
   key :intent, String
+
+  has_attached_file :apk
+  key :apk_file_name, String
 
   timestamps!
 
