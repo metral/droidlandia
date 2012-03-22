ab7752d3ca850bba199b060923db0ed34c6721b2
Mike Metral <mdmetra@sandia.gov>
2012-03-21 23:26:05 -0700

    Added initial adb commands

--- app/workers/adb_worker.rb
+++ app/workers/adb_worker.rb
@@ -3,8 +3,19 @@ class AdbWorker
 
   def self.perform(apk_bin_id)
     apk_bin = ApkBin.find(apk_bin_id)
+    puts "--------------------------------------------------"
     puts "Launching apk File: #{apk_bin.apk.path}  Intent: #{apk_bin.intent}"
-    %x(touch /tmp/bob_sucks)
-    #%x(dscp kn[1-520] #{apk_bin.apk.path})
+
+    adb_connect_cmd = "adb connect " + "10.2.2.12:5555"
+    result = %x(#{adb_connect_cmd})
+    puts "Connect Results: " + result
+
+    adb_install_cmd = "adb install " + apk_bin.apk.path
+    result = %x(#{adb_install_cmd})
+    puts "Install Results: " + result
+
+    adb_disconnect_cmd = "adb disconnect " + "10.2.2.12:5555"
+    result = %x(#{adb_disconnect_cmd})
+    puts "Disconnect Results: " + result
   end
 end
