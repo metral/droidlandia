6c8dd5e92d9ffd53c3449f6eda530af6918a6996
Mike Metral <mdmetra@sandia.gov>
2012-03-21 20:20:28 -0700

    Removed executable command that isn't being used A.T.M

--- app/workers/adb_worker.rb
+++ app/workers/adb_worker.rb
@@ -3,8 +3,8 @@ class AdbWorker
 
   def self.perform(apk_bin_id)
     apk_bin = ApkBin.find(apk_bin_id)
-    puts "Launcing apk File: #{apk_bin.apk.path}  Intent: #{apk_bin.intent}"
+    puts "Launching apk File: #{apk_bin.apk.path}  Intent: #{apk_bin.intent}"
     %x(touch /tmp/bob_sucks)
-    %x(dscp kn[1-520] #{apk_bin.apk.path})
+    #%x(dscp kn[1-520] #{apk_bin.apk.path})
   end
 end
